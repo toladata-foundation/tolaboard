4f0d4c0639be8b3b74ab1c54c265a81d997f7b0e
twall <tomwall.col@gmail.com>
2016-12-04 19:26:06 -0800

    fixed untracked files

--- ember/node_modules/ember-cli-meta-tags/addon/components/head-tags.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import layout from '../templates/components/head-tags';
-
-export default Ember.Component.extend({
-  tagName: '',
-  headTags: Ember.A([]),
-  layout: layout
-});
