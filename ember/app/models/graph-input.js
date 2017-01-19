ac8acd865a593d5994b45cea9bb6c50d99b849a4
Mahmood Khan <mkhan@mercycorps.org>
2017-01-19 10:51:14 -0800

    defined attributes for a couple more models

--- ember/app/models/graph-input.js
+++ ember/app/models/graph-input.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+    graph: DS.belongsTo('Graph', {inverse: 'graphinputs', async: false}),
+    graphinput: DS.attr('string'),
+    aggregationfunction: DS.attr('string'),
 });
