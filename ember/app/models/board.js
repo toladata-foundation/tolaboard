dde290a955e6c3e1efdb71008244b7f94d6065f6
Mahmood Khan <mkhan@mercycorps.org>
2017-01-19 10:45:57 -0800

    added a new board model and defined attributes for graph-model

--- ember/app/models/board.js
+++ ember/app/models/board.js
@@ -2,6 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     owner: DS.belongsTo('User', {inverse: 'graphs', async: false}),
-    thumbnail: DS.attr('string'),
-    ember_component: DS.attr('string'),
+    title: DS.attr('string'),
 });
