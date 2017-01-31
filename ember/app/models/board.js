dc069bd6a6ee64521afa0fc7404e0d72de995d47
Mahmood Khan <mkhan@mercycorps.org>
2017-01-31 10:21:04 -0800

    added inverse relationship between graph and item

--- ember/app/models/board.js
+++ ember/app/models/board.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     owner: DS.belongsTo('user', {async: false}),
-    items: DS.hasMany('item', {inverse: 'board'}),
+    items: DS.hasMany('item', {inverse: 'board', async: false}),
     title: DS.attr('string'),
     createDate: DS.attr('date'),
     updateDate: DS.attr('date')
