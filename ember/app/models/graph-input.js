64b8405dc1c2d231679e4e3ced3aa1d5ae14f59e
Mahmood Khan <mkhan@mercycorps.org>
2017-01-31 09:38:12 -0800

    added a missing comma

--- ember/app/models/graph-input.js
+++ ember/app/models/graph-input.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-    graph: DS.belongsTo('graph', {inverse: 'graph-input', async: false})
+    graph: DS.belongsTo('graph', {inverse: 'graph-input', async: false}),
     graphinput: DS.attr('string'),
     aggregationfunction: DS.attr('string'),
 });
