2e44f900453f73b5dbca50fb3cc5d259183dddc2
Mahmood Khan <mkhan@mercycorps.org>
2017-02-14 10:27:46 -0800

    changed references to helpers to include dashes

--- ember/app/models/boardsilo.js
+++ ember/app/models/boardsilo.js
@@ -3,7 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
 	description: DS.attr('string'),
-  create_date: DS.attr('date'),
   silodata: DS.attr('string'),
   public: DS.attr('boolean')
 });
