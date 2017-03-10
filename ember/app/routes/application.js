2dafee885cf6c93ebf8ebb9b7185ad5843c449be
Mahmood Khan <mkhan@mercycorps.org>
2017-03-09 21:24:31 -0800

    login upon app launch

--- ember/app/routes/application.js
+++ ember/app/routes/application.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
+//import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend({
-    session: Ember.inject.service(),
+    session: Ember.inject.service('session'),
 
     beforeModel() {
         this.get('session').authenticate('authenticator:drf-token-authenticator');
